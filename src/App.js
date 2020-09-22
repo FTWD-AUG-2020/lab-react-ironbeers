a2e45e94c04521de2c0271a1f0985093b27dcbca
matheus <mtava002@gmail.com>
2020-09-22 16:23:56 -0400

    Matheus

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
 import Beers from './components/Beers';
 import HomePage from './components/HomePage';
+import Axios from 'axios'
 
 function App() {
+
+  let [beers, setBeers] = useState([])
+  let [loading, setLoading] = useState(true)
+  useEffect(() => {
+    async function getData() {
+      let res = await Axios.get("https://ih-beers-api2.herokuapp.com/beers")
+      console.log(res.data)
+      setBeers(res.data)
+      setLoading(false)
+    }
+    getData()
+  }, [])
   return (
     <div className="App">
+      {loading ? "loading..." : null}
       <Switch>
         <Route path="/HomePage" render={() => <HomePage />} />
-        <Route path="/Beers" render={() => <Beers />} />
+        <Route path="/Beers" render={(props) => <Beers beers={beers} {...props} />} />
         <Route path="/NewBeer" render={() => <NewBeer />} />
         <Route path="/RandomBeer" render={() => <RandomBeer />} />
       </Switch>

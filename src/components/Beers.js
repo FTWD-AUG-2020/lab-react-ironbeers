5d60d71660bcaf377fa842e61a073f23ce2b26b2
Fabian Pena <camilo.fabis@gmail.com>
2020-09-22 18:28:03 -0400

    Final commit

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Home from '../images/home.png';
+import axios from 'axios';
 
 function Beers(props) {
   let beers = props.beers.map((eachBeer) => {
@@ -14,11 +15,34 @@ function Beers(props) {
       </div>
     );
   });
+
+  const [searchBeer, setSearchBeer] = useState([])
+
+  async function beerLookUp(e) {
+    console.log(e.target.value)
+    let res = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${e.target.value}`)
+    console.log(res)
+    setSearchBeer(res.data)
+  }
+
+  const showBeer = () => {
+    return (
+      searchBeer.map((beer) => {
+        return (
+          <li><Link to={`/Beers/${beer._id}`}>{beer.name}</Link></li>
+        )
+      })
+    )
+  }
+
   return (
     <div>
       <Link to="/HomePage">
-        <img src={Home} alt="home-image"></img>
+        <img src={Home} alt="home-image"/>
       </Link>
+      {showBeer()}
+      <br></br>
+      <input onChange={beerLookUp} type="text" name="search" />
       {beers}
     </div>
   );

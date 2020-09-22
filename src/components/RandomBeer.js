8a8bb0b1b02a88bd0da4f0049b10e018d48920eb
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-22 16:03:11 -0400

    becca's hard work

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Home from '../images/home.png';
 
 function RandomBeer(props) {
-    return (
-        <div>
-            RandomBeer
-        </div>
-    );
+  return (
+    <div>
+      Beers
+      <Link to="/HomePage">
+        <img src={Home} alt="home-image"></img>
+      </Link>
+    </div>
+  );
 }
 
 export default RandomBeer;
